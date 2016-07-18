dc1154611b61dbc8f673c60e18d217d42ef7bcfe
童昭 <liushuang.ls@alibaba-inc.com>
2016-07-18 11:45:03 +0800

    change template

---
+++ src/main/java/ren/liushuang/LiushuangWebApplication.java
@@ -0,0 +1,20 @@
+package ren.liushuang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class LiushuangWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiushuangWebApplication.class, args);
+	}
+
+	@RequestMapping("/index")
+	public String index(){
+		return "Hello Spring boot!";
+	}
+}
--- src/main/java/ren/liushuang/controller/IndexController.java
+++
@@ -1,8 +0,0 @@
-package ren.liushuang.controller;
-
-/**
- * Created by liushuang on 13/7/2016.
- */
-public class IndexController {
-
-}
