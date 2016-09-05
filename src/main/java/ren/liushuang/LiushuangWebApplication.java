d48a40ce900d3ddc5e70edc1f4c3be7ccacd482f
童昭 <liushuang.ls@alibaba-inc.com>
2016-09-05 16:15:23 +0800

    增加bootstrap基本样式；增加nav导航

--- src/main/java/ren/liushuang/LiushuangWebApplication.java
+++ src/main/java/ren/liushuang/LiushuangWebApplication.java
@@ -1,11 +1,7 @@
 package ren.liushuang;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class LiushuangWebApplication {
